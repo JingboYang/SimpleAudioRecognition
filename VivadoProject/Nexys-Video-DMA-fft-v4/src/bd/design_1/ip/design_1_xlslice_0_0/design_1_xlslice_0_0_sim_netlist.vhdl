2fc75f9287f9db7b6e64cf395f03746e95c604f4
LAPTOP-67C4SELT\jingb <jingboeyang@gmail.com>
2018-03-28 11:37:09 -0400

    Add vivdo sources

---
+++ VivadoProject/Nexys-Video-DMA-fft-v4/src/bd/design_1/ip/design_1_xlslice_0_0/design_1_xlslice_0_0_sim_netlist.vhdl
@@ -0,0 +1,37 @@
+-- Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
+-- Date        : Wed Mar 14 13:33:51 2018
+-- Host        : Peter running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode funcsim -rename_top design_1_xlslice_0_0 -prefix
+--               design_1_xlslice_0_0_ design_1_xlslice_0_0_sim_netlist.vhdl
+-- Design      : design_1_xlslice_0_0
+-- Purpose     : This VHDL netlist is a functional simulation representation of the design and should not be modified or
+--               synthesized. This netlist cannot be used for SDF annotated simulation.
+-- Device      : xc7a200tsbg484-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_1_xlslice_0_0 is
+  port (
+    Din : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    Dout : out STD_LOGIC_VECTOR ( 15 downto 0 )
+  );
+  attribute NotValidForBitStream : boolean;
+  attribute NotValidForBitStream of design_1_xlslice_0_0 : entity is true;
+  attribute CHECK_LICENSE_TYPE : string;
+  attribute CHECK_LICENSE_TYPE of design_1_xlslice_0_0 : entity is "design_1_xlslice_0_0,xlslice_v1_0_1_xlslice,{}";
+  attribute DowngradeIPIdentifiedWarnings : string;
+  attribute DowngradeIPIdentifiedWarnings of design_1_xlslice_0_0 : entity is "yes";
+  attribute X_CORE_INFO : string;
+  attribute X_CORE_INFO of design_1_xlslice_0_0 : entity is "xlslice_v1_0_1_xlslice,Vivado 2017.4";
+end design_1_xlslice_0_0;
+
+architecture STRUCTURE of design_1_xlslice_0_0 is
+  signal \^din\ : STD_LOGIC_VECTOR ( 31 downto 0 );
+begin
+  Dout(15 downto 0) <= \^din\(15 downto 0);
+  \^din\(15 downto 0) <= Din(15 downto 0);
+end STRUCTURE;
