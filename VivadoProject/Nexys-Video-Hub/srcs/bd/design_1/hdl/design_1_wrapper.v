e7a1e070f6ffdded391fe69cbaf7a7e4eae2534f
wenxuanqiu <bobbyqiub@gmail.com>
2018-03-29 23:33:46 -0400

    Hub Board Project Source files

---
+++ VivadoProject/Nexys-Video-Hub/srcs/bd/design_1/hdl/design_1_wrapper.v
@@ -0,0 +1,324 @@
+//Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2017.4 (win64) Build 2086221 Fri Dec 15 20:55:39 MST 2017
+//Date        : Fri Mar 23 23:31:35 2018
+//Host        : AnotherPenguin running 64-bit major release  (build 9200)
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   (DDR3_addr,
+    DDR3_ba,
+    DDR3_cas_n,
+    DDR3_ck_n,
+    DDR3_ck_p,
+    DDR3_cke,
+    DDR3_dm,
+    DDR3_dq,
+    DDR3_dqs_n,
+    DDR3_dqs_p,
+    DDR3_odt,
+    DDR3_ras_n,
+    DDR3_reset_n,
+    DDR3_we_n,
+    PWM1,
+    PWM2,
+    SSRon,
+    ja_pin10_io,
+    ja_pin1_io,
+    ja_pin2_io,
+    ja_pin3_io,
+    ja_pin4_io,
+    ja_pin7_io,
+    ja_pin8_io,
+    ja_pin9_io,
+    jb_pin10_io,
+    jb_pin1_io,
+    jb_pin2_io,
+    jb_pin3_io,
+    jb_pin4_io,
+    jb_pin7_io,
+    jb_pin8_io,
+    jb_pin9_io,
+    reset,
+    sys_clock,
+    usb_uart_rxd,
+    usb_uart_txd);
+  output [14:0]DDR3_addr;
+  output [2:0]DDR3_ba;
+  output DDR3_cas_n;
+  output [0:0]DDR3_ck_n;
+  output [0:0]DDR3_ck_p;
+  output [0:0]DDR3_cke;
+  output [1:0]DDR3_dm;
+  inout [15:0]DDR3_dq;
+  inout [1:0]DDR3_dqs_n;
+  inout [1:0]DDR3_dqs_p;
+  output [0:0]DDR3_odt;
+  output DDR3_ras_n;
+  output DDR3_reset_n;
+  output DDR3_we_n;
+  output PWM1;
+  output PWM2;
+  output [7:0]SSRon;
+  inout ja_pin10_io;
+  inout ja_pin1_io;
+  inout ja_pin2_io;
+  inout ja_pin3_io;
+  inout ja_pin4_io;
+  inout ja_pin7_io;
+  inout ja_pin8_io;
+  inout ja_pin9_io;
+  inout jb_pin10_io;
+  inout jb_pin1_io;
+  inout jb_pin2_io;
+  inout jb_pin3_io;
+  inout jb_pin4_io;
+  inout jb_pin7_io;
+  inout jb_pin8_io;
+  inout jb_pin9_io;
+  input reset;
+  input sys_clock;
+  input usb_uart_rxd;
+  output usb_uart_txd;
+
+  wire [14:0]DDR3_addr;
+  wire [2:0]DDR3_ba;
+  wire DDR3_cas_n;
+  wire [0:0]DDR3_ck_n;
+  wire [0:0]DDR3_ck_p;
+  wire [0:0]DDR3_cke;
+  wire [1:0]DDR3_dm;
+  wire [15:0]DDR3_dq;
+  wire [1:0]DDR3_dqs_n;
+  wire [1:0]DDR3_dqs_p;
+  wire [0:0]DDR3_odt;
+  wire DDR3_ras_n;
+  wire DDR3_reset_n;
+  wire DDR3_we_n;
+  wire PWM1;
+  wire PWM2;
+  wire [7:0]SSRon;
+  wire ja_pin10_i;
+  wire ja_pin10_io;
+  wire ja_pin10_o;
+  wire ja_pin10_t;
+  wire ja_pin1_i;
+  wire ja_pin1_io;
+  wire ja_pin1_o;
+  wire ja_pin1_t;
+  wire ja_pin2_i;
+  wire ja_pin2_io;
+  wire ja_pin2_o;
+  wire ja_pin2_t;
+  wire ja_pin3_i;
+  wire ja_pin3_io;
+  wire ja_pin3_o;
+  wire ja_pin3_t;
+  wire ja_pin4_i;
+  wire ja_pin4_io;
+  wire ja_pin4_o;
+  wire ja_pin4_t;
+  wire ja_pin7_i;
+  wire ja_pin7_io;
+  wire ja_pin7_o;
+  wire ja_pin7_t;
+  wire ja_pin8_i;
+  wire ja_pin8_io;
+  wire ja_pin8_o;
+  wire ja_pin8_t;
+  wire ja_pin9_i;
+  wire ja_pin9_io;
+  wire ja_pin9_o;
+  wire ja_pin9_t;
+  wire jb_pin10_i;
+  wire jb_pin10_io;
+  wire jb_pin10_o;
+  wire jb_pin10_t;
+  wire jb_pin1_i;
+  wire jb_pin1_io;
+  wire jb_pin1_o;
+  wire jb_pin1_t;
+  wire jb_pin2_i;
+  wire jb_pin2_io;
+  wire jb_pin2_o;
+  wire jb_pin2_t;
+  wire jb_pin3_i;
+  wire jb_pin3_io;
+  wire jb_pin3_o;
+  wire jb_pin3_t;
+  wire jb_pin4_i;
+  wire jb_pin4_io;
+  wire jb_pin4_o;
+  wire jb_pin4_t;
+  wire jb_pin7_i;
+  wire jb_pin7_io;
+  wire jb_pin7_o;
+  wire jb_pin7_t;
+  wire jb_pin8_i;
+  wire jb_pin8_io;
+  wire jb_pin8_o;
+  wire jb_pin8_t;
+  wire jb_pin9_i;
+  wire jb_pin9_io;
+  wire jb_pin9_o;
+  wire jb_pin9_t;
+  wire reset;
+  wire sys_clock;
+  wire usb_uart_rxd;
+  wire usb_uart_txd;
+
+  design_1 design_1_i
+       (.DDR3_addr(DDR3_addr),
+        .DDR3_ba(DDR3_ba),
+        .DDR3_cas_n(DDR3_cas_n),
+        .DDR3_ck_n(DDR3_ck_n),
+        .DDR3_ck_p(DDR3_ck_p),
+        .DDR3_cke(DDR3_cke),
+        .DDR3_dm(DDR3_dm),
+        .DDR3_dq(DDR3_dq),
+        .DDR3_dqs_n(DDR3_dqs_n),
+        .DDR3_dqs_p(DDR3_dqs_p),
+        .DDR3_odt(DDR3_odt),
+        .DDR3_ras_n(DDR3_ras_n),
+        .DDR3_reset_n(DDR3_reset_n),
+        .DDR3_we_n(DDR3_we_n),
+        .PWM1(PWM1),
+        .PWM2(PWM2),
+        .SSRon(SSRon),
+        .ja_pin10_i(ja_pin10_i),
+        .ja_pin10_o(ja_pin10_o),
+        .ja_pin10_t(ja_pin10_t),
+        .ja_pin1_i(ja_pin1_i),
+        .ja_pin1_o(ja_pin1_o),
+        .ja_pin1_t(ja_pin1_t),
+        .ja_pin2_i(ja_pin2_i),
+        .ja_pin2_o(ja_pin2_o),
+        .ja_pin2_t(ja_pin2_t),
+        .ja_pin3_i(ja_pin3_i),
+        .ja_pin3_o(ja_pin3_o),
+        .ja_pin3_t(ja_pin3_t),
+        .ja_pin4_i(ja_pin4_i),
+        .ja_pin4_o(ja_pin4_o),
+        .ja_pin4_t(ja_pin4_t),
+        .ja_pin7_i(ja_pin7_i),
+        .ja_pin7_o(ja_pin7_o),
+        .ja_pin7_t(ja_pin7_t),
+        .ja_pin8_i(ja_pin8_i),
+        .ja_pin8_o(ja_pin8_o),
+        .ja_pin8_t(ja_pin8_t),
+        .ja_pin9_i(ja_pin9_i),
+        .ja_pin9_o(ja_pin9_o),
+        .ja_pin9_t(ja_pin9_t),
+        .jb_pin10_i(jb_pin10_i),
+        .jb_pin10_o(jb_pin10_o),
+        .jb_pin10_t(jb_pin10_t),
+        .jb_pin1_i(jb_pin1_i),
+        .jb_pin1_o(jb_pin1_o),
+        .jb_pin1_t(jb_pin1_t),
+        .jb_pin2_i(jb_pin2_i),
+        .jb_pin2_o(jb_pin2_o),
+        .jb_pin2_t(jb_pin2_t),
+        .jb_pin3_i(jb_pin3_i),
+        .jb_pin3_o(jb_pin3_o),
+        .jb_pin3_t(jb_pin3_t),
+        .jb_pin4_i(jb_pin4_i),
+        .jb_pin4_o(jb_pin4_o),
+        .jb_pin4_t(jb_pin4_t),
+        .jb_pin7_i(jb_pin7_i),
+        .jb_pin7_o(jb_pin7_o),
+        .jb_pin7_t(jb_pin7_t),
+        .jb_pin8_i(jb_pin8_i),
+        .jb_pin8_o(jb_pin8_o),
+        .jb_pin8_t(jb_pin8_t),
+        .jb_pin9_i(jb_pin9_i),
+        .jb_pin9_o(jb_pin9_o),
+        .jb_pin9_t(jb_pin9_t),
+        .reset(reset),
+        .sys_clock(sys_clock),
+        .usb_uart_rxd(usb_uart_rxd),
+        .usb_uart_txd(usb_uart_txd));
+  IOBUF ja_pin10_iobuf
+       (.I(ja_pin10_o),
+        .IO(ja_pin10_io),
+        .O(ja_pin10_i),
+        .T(ja_pin10_t));
+  IOBUF ja_pin1_iobuf
+       (.I(ja_pin1_o),
+        .IO(ja_pin1_io),
+        .O(ja_pin1_i),
+        .T(ja_pin1_t));
+  IOBUF ja_pin2_iobuf
+       (.I(ja_pin2_o),
+        .IO(ja_pin2_io),
+        .O(ja_pin2_i),
+        .T(ja_pin2_t));
+  IOBUF ja_pin3_iobuf
+       (.I(ja_pin3_o),
+        .IO(ja_pin3_io),
+        .O(ja_pin3_i),
+        .T(ja_pin3_t));
+  IOBUF ja_pin4_iobuf
+       (.I(ja_pin4_o),
+        .IO(ja_pin4_io),
+        .O(ja_pin4_i),
+        .T(ja_pin4_t));
+  IOBUF ja_pin7_iobuf
+       (.I(ja_pin7_o),
+        .IO(ja_pin7_io),
+        .O(ja_pin7_i),
+        .T(ja_pin7_t));
+  IOBUF ja_pin8_iobuf
+       (.I(ja_pin8_o),
+        .IO(ja_pin8_io),
+        .O(ja_pin8_i),
+        .T(ja_pin8_t));
+  IOBUF ja_pin9_iobuf
+       (.I(ja_pin9_o),
+        .IO(ja_pin9_io),
+        .O(ja_pin9_i),
+        .T(ja_pin9_t));
+  IOBUF jb_pin10_iobuf
+       (.I(jb_pin10_o),
+        .IO(jb_pin10_io),
+        .O(jb_pin10_i),
+        .T(jb_pin10_t));
+  IOBUF jb_pin1_iobuf
+       (.I(jb_pin1_o),
+        .IO(jb_pin1_io),
+        .O(jb_pin1_i),
+        .T(jb_pin1_t));
+  IOBUF jb_pin2_iobuf
+       (.I(jb_pin2_o),
+        .IO(jb_pin2_io),
+        .O(jb_pin2_i),
+        .T(jb_pin2_t));
+  IOBUF jb_pin3_iobuf
+       (.I(jb_pin3_o),
+        .IO(jb_pin3_io),
+        .O(jb_pin3_i),
+        .T(jb_pin3_t));
+  IOBUF jb_pin4_iobuf
+       (.I(jb_pin4_o),
+        .IO(jb_pin4_io),
+        .O(jb_pin4_i),
+        .T(jb_pin4_t));
+  IOBUF jb_pin7_iobuf
+       (.I(jb_pin7_o),
+        .IO(jb_pin7_io),
+        .O(jb_pin7_i),
+        .T(jb_pin7_t));
+  IOBUF jb_pin8_iobuf
+       (.I(jb_pin8_o),
+        .IO(jb_pin8_io),
+        .O(jb_pin8_i),
+        .T(jb_pin8_t));
+  IOBUF jb_pin9_iobuf
+       (.I(jb_pin9_o),
+        .IO(jb_pin9_io),
+        .O(jb_pin9_i),
+        .T(jb_pin9_t));
+endmodule
